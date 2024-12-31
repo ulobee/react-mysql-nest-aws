f809d0ad33c6a47a7c5489f786aebeda47ad8381
medfadiabaidi1 <medfadiabaidi1@gmail.com>
2024-12-31 13:32:36 +0100

    ee

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true, // Allow all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
     await app.listen(8080);
